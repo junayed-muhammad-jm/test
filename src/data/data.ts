118f3c70d1f8b03b7b1af0159ba1f7068481b094
junayed-muhammad-jm <junayedmuhammad.jm@gmail.com>
2025-10-05 15:56:42 +0600

    new update

--- src/data/data.ts
+++ src/data/data.ts
@@ -9,84 +9,84 @@ export interface DataImage {
 const ImageData: DataImage[] = [
   {
     id: 1,
-    image: "public/i-1.jpg",
+    image: "/i-1.jpg",
     writer: "মাওলানা মহিউদ্দীন খান ",
     name: "জীবনের খেলা ঘরে",
     src: "https://www.rokomari.com/book/44455/jiboner-khalagore",
   },
   {
     id: 2,
-    image: "public/i-2.jpg",
+    image: "/i-2.jpg",
     writer: "মাহবুব উল্লাহ",
     name: "আমার জীবন আমার সংগ্রাম। ",
     src: "https://www.rokomari.com/book/357006/my-life-my-struggle",
   },
   {
     id: 3,
-    image: "public/i-3.jpg",
+    image: "/i-3.jpg",
     writer: "জন গডেন",
     name: "বাংলার রোদে আমরা দু’বোন।",
     src: "https://www.rokomari.com/book/484938/banglar-rode-amra-du-bon",
   },
   {
     id: 4,
-    image: "public/i-4.jpg",
+    image: "/i-4.jpg",
     writer: "মীর মোশারফ হোসেন ",
     name: "আমার জীবনি। ",
     src: "https://www.rokomari.com/book/387081/amer-jiboni",
   },
   {
     id: 5,
-    image: "public/i-5.jpg",
+    image: "/i-5.jpg",
     writer: "ড এনামুক হক",
     name: "কারুময় আত্মার ঝাঁপিতে  ",
     src: "https://www.rokomari.com/book/237937/karumoy-atmar-jhanpite",
   },
   {
     id: 6,
-    image: "public/i-6.jpg",
+    image: "/i-6.jpg",
     writer: "ড মুহাম্মদ ইব্রাহিম ",
     name: "জীবনস্মৃতিতে মানুষ দেশ বিজ্ঞান । ",
     src: "https://www.rokomari.com/book/110732/jibon-smrititee-manush-desh-biggan-1st-khondo",
   },
   {
     id: 7,
-    image: "public/i-7.jpg",
+    image: "/i-7.jpg",
     writer: "ড মুহাম্মদ ইউনুস ",
     name: "গ্রামীণ ব্যাংক ও আমার জীবন । ",
     src: "https://www.rokomari.com/book/75672/grameen-bank-o-amar-jibon",
   },
   {
     id: 8,
-    image: "public/i-8.jpg",
+    image: "/i-8.jpg",
     writer: "আবুল হায়াত ",
     name: "জীবন খাতার ফুটনোট।  ",
     src: "https://www.rokomari.com/book/155863/jibon-khatar-futnot",
   },
   {
     id: 9,
-    image: "public/i-9.jpg",
+    image: "/i-9.jpg",
     writer: "বিভূতিভূষণ বন্দ্যোপাধ্যায়",
     name: "স্মৃতির রেখা।  ",
     src: "https://www.rokomari.com/book/338638/smritir-rekha",
   },
   {
     id: 10,
-    image: "public/i-10.jpg",
+    image: "/i-10.jpg",
     writer: " সুফিয়া খাতুন",
     name: "জীবন নদীর বাঁকে বাঁকে।  ",
     src: "https://www.rokomari.com/book/231310/jibon-nodir-bake-bake",
   },
   {
     id: 11,
-    image: "public/i-11.jpg",
+    image: "/i-11.jpg",
     writer: " ইয়াহইয়া সিনওয়ার",
     name: "কাঁটা ও",
     src: "https://www.rokomari.com/book/435679/kata-o-ful",
   },
   {
     id: 12,
-    image: "public/i-12.jpg",
+    image: "/i-12.jpg",
     writer: "মানিক বন্দ্যপাধ্যায়",
     name: "পদ্মা নদীর মাঝি",
     src: "https://www.rokomari.com/book/99337/padma-nodir-majhi",
@@ -94,7 +94,7 @@ const ImageData: DataImage[] = [
 
   {
     id: 13,
-    image: "public/i-13.jpg",
+    image: "/i-13.jpg",
     writer: "মানিক বন্দ্যপাধ্যায়",
     name: " পুতুল নাচের ইতিকথা ",
     src: "https://www.rokomari.com/book/22497/putulnacher-etikhota",
@@ -102,7 +102,7 @@ const ImageData: DataImage[] = [
 
   {
     id: 14,
-    image: "public/i-14.jpg",
+    image: "/i-14.jpg",
     writer: "আবুল হাসান আলী নদভী ",
     name: "সংগ্রামি সাধকদের ইতিহাস ",
     src: "https://www.wafilife.com/shongrami-shadhoker-itihash/dp/22931",
@@ -110,7 +110,7 @@ const ImageData: DataImage[] = [
 
   {
     id: 15,
-    image: "public/i-15.jpg",
+    image: "/i-15.jpg",
     writer: "আবু তাহের মেসবাহ",
     name: "বাইতুল্লার মুসাফির",
     src: "https://www.wafilife.com/baitullahor-musafir/dp/22264",
@@ -118,7 +118,7 @@ const ImageData: DataImage[] = [
 
   {
     id: 16,
-    image: "public/i-16.jpg",
+    image: "/i-16.jpg",
     writer: "মাওলানা লিয়াকত আলী ",
     name: "ফিলিস্তিন সমস্যা ও ইহুদি চক্রান্ত",
     src: "https://www.rokomari.com/book/212475/filastin-somossa-o-ihudi-chokranto",
@@ -126,7 +126,7 @@ const ImageData: DataImage[] = [
 
   {
     id: 17,
-    image: "public/i-17.jpg",
+    image: "/i-17.jpg",
     writer:
       "কালিমাতুন আন সিরাতিল মুহাদ্দিস ওয়াল ফকিহ আশ শায়খ মুহাম্মাদ আবদুল মালেক আল-কুমিল্লায়ী",
     name: "মুহাম্মদ আবু সায়েম ",
@@ -135,7 +135,7 @@ const ImageData: DataImage[] = [
 
   {
     id: 18,
-    image: "public/i-18.jpg",
+    image: "/i-18.jpg",
     writer: "সাবের চৌধুরি",
     name: "কোথাও নিঝুম হয়েছে কেউ",
     src: "https://www.rokomari.com/book/432939/kothao-nijum-hoyeche-keu",
@@ -143,14 +143,14 @@ const ImageData: DataImage[] = [
 
   {
     id: 19,
-    image: "public/i-19.jpg",
+    image: "/i-19.jpg",
     writer: "জুবায়ের রশীদ",
     name: "বুনোগন্ধের জীবন",
     src: "https://www.rokomari.com/book/478254/bunogondher-jibon",
   },
   {
     id: 20,
-    image: "public/i-20.jpg",
+    image: "/i-20.jpg",
     writer: "জাস্টিস ত্বাকী উসমানি হাফিঃ",
     name: "আমার স্মৃতি কিছু সুখের কিছু দুঃখের",
     src: "https://www.rokomari.com/book/223266/amar-smriti-kichu-sukher-kichu-dukher",
